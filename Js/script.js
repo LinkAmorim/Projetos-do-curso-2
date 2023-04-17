87d1514db8b07af4ed41ed7b390dd8e48bbc7781
Rafael <114101763+KesleyMelchior@users.noreply.github.com>
2023-04-17 11:41:13 -0300

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,84 @@
+// Mudança de texto do botão quando mudada a opção
+let codificar = document.getElementById('codificar');
+let decodificar = document.getElementById('decodificar');
+let botao = document.getElementById('botao');
+
+function change() {
+  if (decodificar.checked) {
+    botao.innerText = "decodificar";
+  } else if (codificar.checked) {
+    botao.innerText = "codificar";
+  }
+}
+
+// mostrar ou ocultar a opção de incremento
+let cesar = document.getElementById('cesar');
+let base64 = document.getElementById('base64');
+let incrementar = document.getElementById('incrementar');
+
+function mostraIncremento() {
+  incrementar.style.display = "block";
+}
+
+function escondeIncremento() {
+  incrementar.style.display = "none";
+}
+
+cesar.addEventListener('change', mostraIncremento);
+base64.addEventListener('change', escondeIncremento);
+
+let resposta = document.getElementById('resultado');
+let incremento = parseInt(document.getElementById('incrementacao').value);
+console.log(incremento);
+console.log(typeof incremento);
+
+botao.addEventListener('click', function (event) {
+  event.preventDefault();
+  opcaoSelecionada();
+});
+
+// Calculos com cifra de cesar e base64
+function codificarCesar(mensagem) {
+  mensagem = mensagem.split("");
+  console.log(mensagem);
+  let mensagemAtual = mensagem.map((valor) => valor.charCodeAt());
+  console.log(mensagemAtual);
+  let mensagemInclemento = mensagemAtual.map((valor) => valor + incremento);
+  console.log(mensagemInclemento);
+  let mensagemNova = mensagemInclemento.map((valor) => String.fromCharCode(valor)).join("");
+  console.log(mensagemNova);
+  return mensagemNova;
+}
+
+function decodificarCesar(mensagem) {
+  mensagem = mensagem.split("");
+  let mensagemAtual = mensagem.map((valor) => valor.charCodeAt())
+  let mensagemInclemento = mensagemAtual.map((valor) => valor - incremento);
+  let mensagemNova = mensagemInclemento.map((valor) => String.fromCharCode(valor)).join("");
+  console.log(mensagemNova);
+  return mensagemNova;
+}
+
+function opcaoSelecionada() {
+  let mensagem = document.getElementById('mensagem').value;
+  console.log(mensagem);
+  if (codificar.checked) {
+    if (cesar.checked) {
+      console.log("Codificar Cesar")
+      resposta.innerText = codificarCesar(mensagem);
+    } else {
+      console.log("Codificar Base64")
+      resposta.innerText = btoa(mensagem);
+    }
+  } else if (decodificar.checked) {
+    if (cesar.checked) {
+      console.log("Decodificar César")
+      resposta.innerText = decodificarCesar(mensagem);
+    } else {
+      console.log("Decodificar base64")
+      resposta.innerText = atob(mensagem);
+    }
+  } else {
+    resposta.innerText = "Digite seu Código ";
+  }
+}
